3b1d80168139a5e97af3fd00c55bbaefa865f93c
Tevinthuku <tevinthuku@gmail.com>
2021-07-10 15:40:44 +0300

    clean up how we get the subscriptions

--- pkg/gamestationplus/members/service_test.go
+++ pkg/gamestationplus/members/service_test.go
@@ -13,6 +13,15 @@ import (
 	. "github.com/smartystreets/goconvey/convey"
 )
 
+func getListOfValidSubscriptionCodes(count int) []subscriptionDomain.SubscriptionCode {
+	subscriptions := inmem.PopulateSubscriptions()
+	codes := make([]subscriptionDomain.SubscriptionCode, count)
+	for i := 0; i < count; i++ {
+		codes[i] = subscriptions[i].Code
+	}
+	return codes
+}
+
 func TestMemberService(t *testing.T) {
 
 	memberRepo := inmem.NewMembersStore()
@@ -21,10 +30,7 @@ func TestMemberService(t *testing.T) {
 	plusSubscriptionRepo := inmem.NewSubscriptionsStore()
 	subscriptionService := subscriptions.NewService(plusSubscriptionRepo)
 	service := NewService(memberRepo, accountsService, subscriptionService)
-	subscriptionCode1 := subscriptionDomain.SubscriptionCode("FR2756290519")
-	subscriptionCode2 := subscriptionDomain.SubscriptionCode("FR7971713142")
-	subscriptionCode3 := subscriptionDomain.SubscriptionCode("CH6944356JHF")
-	subscriptionCode4 := subscriptionDomain.SubscriptionCode("FR6183522024")
+	subscriptionCodes := getListOfValidSubscriptionCodes(5)
 	acc1 := networkDomain.Account{
 		SignInID:    networkDomain.SignInID("test@gamer.com"),
 		UserName:    "testgamer",
@@ -34,23 +40,23 @@ func TestMemberService(t *testing.T) {
 	_, _ = accountsService.CreateAccount(acc1)
 	Convey("Given an OnlineID", t, func() {
 		Convey("it should let the account owner join gamestation plus if the SignInID is registered on gamestation network", func() {
-			member, err := service.JoinToPlayStationPlus(onlineID, acc1.SignInID, subscriptionCode1)
+			member, err := service.JoinToPlayStationPlus(onlineID, acc1.SignInID, subscriptionCodes[0])
 			So(err, ShouldBeNil)
 			So(member.OnlineID, ShouldEqual, onlineID)
 		})
 		Convey("it should not let the user join game station plus if the member's SignInID isnt registered on gamestation network", func() {
 			onlineID := domain.OnlineID("gamer2")
 			signInID := networkDomain.SignInID("test2@gmail.com")
-			_, err := service.JoinToPlayStationPlus(onlineID, signInID, subscriptionCode2)
+			_, err := service.JoinToPlayStationPlus(onlineID, signInID, subscriptionCodes[1])
 			So(err, ShouldNotBeNil)
 		})
 		Convey("it should not let an onlineID be used to join twice", func() {
-			_, err := service.JoinToPlayStationPlus(onlineID, acc1.SignInID, subscriptionCode3)
+			_, err := service.JoinToPlayStationPlus(onlineID, acc1.SignInID, subscriptionCodes[2])
 			So(err, ShouldNotBeNil)
 		})
 		Convey("it should not allow a member to join if SignInID is already registered to a member", func() {
 			onlineID := domain.OnlineID("gamer4")
-			_, err := service.JoinToPlayStationPlus(onlineID, acc1.SignInID, subscriptionCode4)
+			_, err := service.JoinToPlayStationPlus(onlineID, acc1.SignInID, subscriptionCodes[3])
 			So(err, ShouldNotBeNil)
 		})
 	})
